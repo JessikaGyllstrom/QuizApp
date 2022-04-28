aa9d53ed5a2defffa12f747678944228272da9b1
Jessika <72019869+JessikaGyllstrom@users.noreply.github.com>
2022-04-28 17:54:50 +0200

    score added

--- game.js
+++ game.js
@@ -5,6 +5,8 @@ const scoreText = document.getElementById('score');
 const progressBarFull = document.getElementById('progressBarFull');
 const loader = document.getElementById('loader');
 const game = document.getElementById('game');
+const questionCounterText = document.getElementById('questionCounter');
+
 let currentQuestion = {};
 let acceptingAnswers = false;
 let score = 0;
@@ -61,6 +63,7 @@ getNewQuestion = () => {
         return window.location.assign('/end.html');
     }
     questionCounter++;
+    questionCounterText.innerText = `${questionCounter}/${MAX_QUESTIONS}`;
     const questionIndex =Math.floor(Math.random() * availableQuestions.length);
     currentQuestion = availableQuestions[questionIndex];
     question.innerText = currentQuestion.question;
@@ -77,8 +80,20 @@ choices.forEach(choice => {
         acceptingAnswers = false;
         const selectedChoice = e.target;
         const selectedAnswer = selectedChoice.dataset["number"];
-        console.log(selectedAnswer);
-        getNewQuestion();
+        const classToApply = selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
+        if(classToApply === "correct") {
+            incrementScore(CORRECT_BONUS);
+        }
+        selectedChoice.parentElement.classList.add(classToApply);
+        setTimeout( () => {
+            selectedChoice.parentElement.classList.remove(classToApply);
+            getNewQuestion();
+        }, 1000);
+        console.log(classToApply);
     })
 })
+incrementScore = num => {
+    score += num;
+    scoreText.innerText = score;
+}
 startGame();
